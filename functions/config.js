86b5b46082b79465f1538e7a8532c7874867f9b0
bahoocoding <85931441+bahoocoding@users.noreply.github.com>
2021-07-01 10:01:21 +0700

    Delete config.js

--- functions/config.js
+++
@@ -1,7 +0,0 @@
-module.exports= {
-    JWT_SECRET: process.env.JWT_SECRET || "hungnguyen196",
-    GOOGLE_USER: process.env.GOOGLE_USER || "bahoocoding@gmail.com",
-    GOOGLE_PASSWORD: process.env.GOOGLE_PASSWORD || "@hungnguyen196",
-    DOMAIN: process.env.DOMAIN || "http://localhost:3000",
-  };
-  
